ec78343db257a01244e20c1ca047ae2df6addfc3
Tagforaz <nasibshahverdiyev@gmail.com>
2026-06-29 11:25:14 +0400

    Initial Commit

---
+++ src/Core/SilentMoon.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SilentMoon.Domain.Entities
+{
+    public class ApplicationUser
+    {
+        public ApplicationUser()
+        {
+            Pomodoros = new HashSet<Pomodoro>();
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? RefreshTokenId { get; set; }
+        public RefreshToken RefreshToken { get; set; }
+        public virtual ICollection<Pomodoro> Pomodoros { get; set; }
+    }
+
+}
